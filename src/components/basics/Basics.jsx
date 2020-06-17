378bdc325017a77d691620d3bd086b77230f0d51
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-06-16 20:48:42 -0400

    trying to get firebase database to render

--- src/components/basics/Basics.jsx
+++ src/components/basics/Basics.jsx
@@ -1,14 +1,42 @@
-import React, { useState } from 'react';
-import data from "./basicdata";
+import React, { useEffect, useState } from 'react';
+// import data from "./basicdata";
+import { firestore } from '../../auth/firebase';
 import BasicsList from "./BasicsList";
 
-function Basics(props) {
-    const [tips] = useState(data);
+
+// function Basics(props) {
+//     const [tips] = useState(data);
+  
+//     return (
+//         <div>
+//             <BasicsList items={tips} />
+//         </div>
+//     )
+// }
+
+const Basics = () => {
+    const [tipsList, setTipsList] = useState([]);
+
+    useEffect(() => {
+        firestore()
+        .get("Basics")
+        .then((res) => {
+            console.log(res.data);
+            setTipsList(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }, []);
+
     return (
         <div>
-            <BasicsList items={tips} />
+            <BasicsList items={tipsList} />
         </div>
     )
-}
+};
+
+
+
 
 export default Basics;
