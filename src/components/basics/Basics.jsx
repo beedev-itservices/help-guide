b08e899e7e04fc4800dd460efbd377362fb5c92f
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-06-16 21:02:06 -0400

    removed firebase for data

--- src/components/basics/Basics.jsx
+++ src/components/basics/Basics.jsx
@@ -1,42 +1,16 @@
 import React, { useEffect, useState } from 'react';
-// import data from "./basicdata";
-import { firestore } from '../../auth/firebase';
+import data from "./basicdata";
 import BasicsList from "./BasicsList";
 
 
-// function Basics(props) {
-//     const [tips] = useState(data);
+function Basics(props) {
+    const [tips] = useState(data);
   
-//     return (
-//         <div>
-//             <BasicsList items={tips} />
-//         </div>
-//     )
-// }
-
-const Basics = () => {
-    const [tipsList, setTipsList] = useState([]);
-
-    useEffect(() => {
-        firestore()
-        .get("Basics")
-        .then((res) => {
-            console.log(res.data);
-            setTipsList(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    }, []);
-
     return (
         <div>
-            <BasicsList items={tipsList} />
+            <BasicsList items={tips} />
         </div>
     )
-};
-
-
-
+}
 
 export default Basics;
--- src/components/basics/BasicsList.jsx
+++ src/components/basics/BasicsList.jsx
@@ -1,107 +1,16 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { firestore } from '../../auth/firebase';
-import { useRouteMatch, useHistory } from "react-router-dom";
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-// function BasicsList(props) {
-//   const { goBack } = useHistory();
-//   console.log("ml: basicslist.jsx: basicslist: ", props);
-
-// const initialTip = {
-//   num: "",
-//   name: "",
-//   info01: "",
-//   info02: "",
-//   info03: "",
-//   info04: "",
-// };
-const tips = firestore.collection('Basis');
-
-const BasicsList = ({ tips }) => {
-  const match = useRouteMatch();
-  const history = useHistory();
-  console.log(tips);
-  // const [editing, setEditing] = useState(false);
-  // const [tipToEdit, setTipToEdit] = useState(initialTip);
-  // const [newTip, setNewTip] = useState({
-  //   num: "",
-  //   name: "",
-  //   info01: "",
-  //   info02: "",
-  //   info03: "",
-  //   info04: "",
-  // });
-
-  // const editTip = tip => {
-  //   setEditing(true);
-  //   setTipToEdit(tip);
-  // };
-
-  // const saveEdit = e => {
-  //   e.preventDefault();
-  //   console.log(tipToEdit)
-  //   firestore()
-  //     .put(`Basics/${document.id}`, tipToEdit)
-  //     .then(res => {
-  //       console.log(res);
-  //       console.log(res.data);
-  //       firestore().get('Basics')
-  //         .then(res => {
-  //           updateTips(res.data)
-  //         })
-  //         .catch(err => console.log(err))
-  //         console.log(res.data.payload);
-  //       history.push(`/`)
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     })
-  // };
-
-  // const deleteTip = tip => {
-  //   firestore()
-  //     .delete(`Basics/${document.id}`, tip)
-  //     .then(res => {
-  //       console.log(res);
-  //       console.log(res.data);
-  //       firestore().get('Basics')
-  //         .then(res => {
-  //           updateTips(res.data)
-  //         })
-  //         .catch(err => console.log(err))
-  //         console.log(res.data.payload);
-  //         history.push(`/`)
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     })
-  // };
-
-  // const addTip = (e) => {
-  //   e.preventDefault();
-  //   console.log(newTip);
-  //   firestore()
-  //   .post('Basics', newTip)
-  //   .then(res => {
-  //     updateTips(res.data)
-  //   })
-  //   .catch(err => console.log(err))
-  //   console.log(res.data.payload);
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-  // };
-
-  // const handleChange = (e) => {
-  //   setNewTip({...newTip, [e.target.name]: e.target.value})
-  // }
+function BasicsList(props) {
+  const { goBack } = useHistory();
+  console.log("ml: basicslist.jsx: basicslist: ", props);
 
 
   return (
     <div className="guide-wrapper">
       <div className="guide">
         <div className="guide-header">
-          {/* <button onClick={() => goBack()}>Go Back</button> */}
+          <button onClick={() => goBack()}>Go Back</button>
           <h1>The Basics Guides</h1>
         </div>
         <div className="guide-words">
@@ -111,15 +20,15 @@ const BasicsList = ({ tips }) => {
         </div>
       </div>
       <div className="the-items">
-        {tips.map(Basics => (
-          <div className="item-card" key={Basics.id}>
-          <h3>{Basics.num}</h3>
-          <h3>{Basics.name}</h3>
+        {props.items.map(item => (
+          <div className="item-card" key={item.id}>
+          <h3>{item.num}</h3>
+          <h3>{item.name}</h3>
           <div className="item-info">
-            <p>{Basics.info01}</p>
-            <p>{Basics.info02}</p>
-            <p>{Basics.info03}</p>
-            <p>{Basics.info04}</p>
+            <p>{item.info01}</p>
+            <p>{item.info02}</p>
+            <p>{item.info03}</p>
+            <p>{item.info04}</p>
           </div>
       </div>
         ))}
