e3fee2ca587bff72d480b555a77d8fa851b9193c
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-09-13 20:52:30 -0400

    pushing unsaved changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './css/App.css';
 import Home from "./components/Home";
 import { Route, Link, Switch } from "react-router-dom";
